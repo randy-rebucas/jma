bbb076b27939e1af0f85e1d799848cb3e0468660
randy <rebucasrandy1986@gmail.com>
2024-02-18 09:23:18 +0800

    add setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +23,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (Schema::hasTable('settings')) {
+            if (!Config('settings')) {
+                foreach (Setting::all()->pluck('value', 'key') as $k => $val) {
+                    config(['settings.' . $k => $val]);
+                }
+            }
+        }
+        
+        
 
         Builder::macro("search", function ($field, $search) {
             return $search ? $this->where($field, 'like', '%' . $search . '%') : $this;
--- resources/views/livewire/pages/setting/manage-settings.blade.php
+++ resources/views/livewire/pages/setting/manage-settings.blade.php
@@ -1,18 +1,35 @@
 <?php
 
+use App\Models\Setting;
 use Livewire\Volt\Component;
 
-new class extends Component
-{
+new class extends Component {
+
+    public $setting = [];
+
+    public function mount()
+    {
+        $this->setting['business_name'] = config('settings.business_name');
+        $this->setting['business_contact'] = config('settings.business_contact');
+    }
+
     /**
      * Delete the sale.
      */
-    public function delete(Sale $id): void
+    public function save()
     {
-        $sale = Sale::find($id);
-        $sale->delete();
-        // $this->emit('');
-        // $this->close();
+        foreach ($this->setting as $key => $value) {
+            $item = Setting::where('key', $key)->first();
+
+            if (empty($item)) {
+                Setting::create([
+                    'key' => $key,
+                    'value' => $value,
+                ]);
+            } else {
+                Setting::where('key', $key)->update(['value' => $value]);
+            }
+        }
     }
 }; ?>
 
@@ -27,6 +44,17 @@ new class extends Component
         </p>
     </header>
     <div class="mt-6 space-y-6">
-        Setting table
+        <form wire:submit="save">
+
+            <x-input-label for="business_name" :value="__('Business Name')" />
+            <x-text-input wire:model="setting.business_name" id="business_name" class="block mt-1 w-full" type="text" />
+            
+            <x-input-label for="business_contact" :value="__('Business Contact')" />
+            <x-text-input wire:model="setting.business_contact" id="business_contact" class="block mt-1 w-full" type="text" />
+
+            <x-primary-button class="my-4" wire:loading.attr="disabled">
+                {{ __('Save') }}
+            </x-primary-button>
+        </form>
     </div>
 </section>
