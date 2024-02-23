770732b75d1a7b688e762f0df93325ffef196140
randy <rebucasrandy1986@gmail.com>
2024-02-24 01:19:06 +0800

    update

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -11,6 +11,13 @@ class Dashboard extends Component
     
     public function render()
     {
+        // return view('dashboard.index', [
+        //     'total_paid' => Order::sum('pay'),
+        //     'total_due' => Order::sum('due'),
+        //     'complete_orders' => Order::where('order_status', 'complete')->get(),
+        //     'products' => Product::orderBy('product_store')->take(5)->get(),
+        //     'new_products' => Product::orderBy('buying_date')->take(2)->get(),
+        // ]);
         return view('livewire.dashboard');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $admin = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+        ]);
+        // Employee::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Permission::create(['name' => 'pos.menu', 'group_name' => 'pos']);
+        // Permission::create(['name' => 'employee.menu', 'group_name' => 'employee']);
+        Permission::create(['name' => 'customer.menu', 'group_name' => 'customer']);
+        Permission::create(['name' => 'supplier.menu', 'group_name' => 'supplier']);
+        Permission::create(['name' => 'item.menu', 'group_name' => 'item']);
+        Permission::create(['name' => 'sale.menu', 'group_name' => 'sale']);
+        Permission::create(['name' => 'receiving.menu', 'group_name' => 'receiving']);
+        Permission::create(['name' => 'report.menu', 'group_name' => 'report']);
+        Permission::create(['name' => 'setting.menu', 'group_name' => 'setting']);
+        Permission::create(['name' => 'roles.menu', 'group_name' => 'roles']);
+        Permission::create(['name' => 'user.menu', 'group_name' => 'user']);
+
+        Role::create(['name' => 'SuperAdmin'])->givePermissionTo(Permission::all());
+        Role::create(['name' => 'Admin'])->givePermissionTo(['customer.menu', 'user.menu', 'supplier.menu']);
+        Role::create(['name' => 'Account'])->givePermissionTo(['customer.menu', 'user.menu', 'supplier.menu']);
+        Role::create(['name' => 'Manager'])->givePermissionTo(['item.menu', 'sale.menu', 'receiving.menu', 'report.menu']);
+
+        $admin->assignRole('SuperAdmin');
+
         $this->call([
             CountrySeeder::class,
             CitySeeder::class,
